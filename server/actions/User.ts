f0b9452c86cfde5c37c0042c9329f09a7d429906
Kemal Fidan <kemalfidan3@gmail.com>
2021-01-19 00:22:00 -0500

    changes to test workflows

--- server/actions/User.ts
+++ server/actions/User.ts
@@ -1,5 +1,5 @@
 export async function getAlteredUsername(username: string): Promise<string> {
   if (!username) throw new Error("Invalid username");
-
+  console.log("testing for pr");
   return username + "123";
 }
