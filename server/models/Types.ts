19f02e0afc216219f94c51c20507dbddcb467959
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2021-02-07 02:27:22 -0500

    Added basic outline of types objects

--- server/models/Types.ts
+++ server/models/Types.ts
@@ -1,4 +1,30 @@
 
 export interface Types {
+    
+    id: {}
 
+    name: {}
+
+    description: {}
+
+    time: {
+        start_time: {}
+        end_time: {}
+        date: {}
+        hours: {}
+    }
+
+    location: {}
+
+    start_registration: {}
+
+    end_registration: {}
+
+    image: {}
+
+    attendees: {
+        registered_attendees: {}
+        present_attendees: {}
+        absent_attendees: {}
+    }
 }
