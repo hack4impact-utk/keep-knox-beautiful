451dda326d8d310a8ee44d28ac606132bf4970f7
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2021-02-07 21:20:48 -0500

    Changed format of types, added datatypes to each

--- server/models/Types.ts
+++ server/models/Types.ts
@@ -1,30 +1,18 @@
+import { ObjectID } from "mongodb";
 
 export interface Types {
-    
-    id: {}
-
-    name: {}
-
-    description: {}
-
-    time: {
-        start_time: {}
-        end_time: {}
-        date: {}
-        hours: {}
-    }
-
-    location: {}
-
-    start_registration: {}
-
-    end_registration: {}
-
-    image: {}
-
-    attendees: {
-        registered_attendees: {}
-        present_attendees: {}
-        absent_attendees: {}
-    }
+  _id: ObjectID;
+  name: string;
+  description?: string;
+  date?: Date;
+  start_time?: Date;
+  end_time?: Date;
+  location?: string;
+  start_registration?: boolean;
+  end_registration?: boolean;
+  hours?: number;
+  image?: File;
+  registered_attendees?: Array<string>;
+  present_attendees?: Array<string>;
+  absent_attendees?: Array<string>;
 }
