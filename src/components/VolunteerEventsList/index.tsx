bf8aa4cffe7c5602efea8d8e919d43f281b77548
Ross Ketron <rossketron@gmail.com>
2021-03-08 16:09:16 -0500

    updated pagination to more minimal design

--- src/components/VolunteerEventsList/index.tsx
+++ src/components/VolunteerEventsList/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Event, Volunteer } from "utils/types";
 import CoreTypography from "src/components/core/typography/CoreTypography";
 import colors from "src/components/core/colors";
-import { createStyles, makeStyles, Theme, useTheme, withStyles } from "@material-ui/core/styles";
+import { createStyles, makeStyles, Theme, withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -11,78 +11,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import SortIcon from "@material-ui/icons/Sort";
-import TablePagination from "@material-ui/core/TablePagination";
-import IconButton from "@material-ui/core/IconButton";
-import FirstPageIcon from "@material-ui/icons/FirstPage";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import LastPageIcon from "@material-ui/icons/LastPage";
-
-// create styles for table pagination footer
-const useStyles1 = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexShrink: 0,
-            marginLeft: theme.spacing(2.5),
-        },
-    })
-);
-
-// create props for table pagination
-interface TablePaginationActionsProps {
-    count: number;
-    page: number;
-    rowsPerPage: number;
-    onChangePage: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
-}
-
-// create table pagination actions and first, prev, next, last page buttons
-function TablePaginationActions(props: TablePaginationActionsProps) {
-    const classes = useStyles1();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
-
-    const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, 0);
-    };
-
-    const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <div className={classes.root}>
-            <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="first page">
-                {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-                {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </div>
-    );
-}
+import Pagination from "@material-ui/lab/Pagination";
 
 // style attended event table header row cells
 const StyledTableCell = withStyles((theme: Theme) =>
@@ -155,20 +84,17 @@ const rows = [
 // create attended events table for current volunteer
 export default function VolunteerEventsList(props: Volunteer) {
     const classes = useStyles();
-    const [page, setPage] = useState(0);
-    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const eventsPerPage = 5;
+    const [page, setPage] = useState(1);
 
-    // Uncomment to use actual volunteer data for volId from server
-    // const rows: Array<Event> = props.attendedEvents ? props.attendedEvents : [];
-
-    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+    const handleChange = (event: React.ChangeEvent<unknown>, newPage: number) => {
         setPage(newPage);
     };
 
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+    // Uncomment to use actual volunteer data for volId from server
+    //const rows: Array<Event> = props.attendedEvents ? props.attendedEvents : [];
+
+    const numPages = rows.length > 0 ? Math.ceil(rows.length / eventsPerPage) : 0;
 
     return (
         <TableContainer component={Paper} className={classes.tableContainer}>
@@ -193,46 +119,34 @@ export default function VolunteerEventsList(props: Volunteer) {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {(rowsPerPage > 0 ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : rows).map(
-                        row => (
-                            <TableRow key={row.name} className={classes.eventRow}>
-                                <TableCell component="th" scope="row">
-                                    <CoreTypography variant="body2">{row.name}</CoreTypography>
-                                </TableCell>
-                                <TableCell align="center">
-                                    <CoreTypography variant="body2">
-                                        {row.endDate?.toLocaleDateString("en-us", {
-                                            month: "2-digit",
-                                            day: "2-digit",
-                                            year: "numeric",
-                                        })}
-                                    </CoreTypography>
-                                </TableCell>
-                                <TableCell align="center">
-                                    <CoreTypography variant="body2">{row.hours}</CoreTypography>
-                                </TableCell>
-                            </TableRow>
-                        )
-                    )}
+                    {(numPages > 0
+                        ? rows.slice((page - 1) * eventsPerPage, (page - 1) * eventsPerPage + eventsPerPage)
+                        : rows
+                    ).map(row => (
+                        <TableRow key={row.name} className={classes.eventRow}>
+                            <TableCell component="th" scope="row">
+                                <CoreTypography variant="body2">{row.name}</CoreTypography>
+                            </TableCell>
+                            <TableCell align="center">
+                                <CoreTypography variant="body2">
+                                    {row.endDate?.toLocaleDateString("en-us", {
+                                        month: "2-digit",
+                                        day: "2-digit",
+                                        year: "numeric",
+                                    })}
+                                </CoreTypography>
+                            </TableCell>
+                            <TableCell align="center">
+                                <CoreTypography variant="body2">{row.hours}</CoreTypography>
+                            </TableCell>
+                        </TableRow>
+                    ))}
                 </TableBody>
             </Table>
             <div className={classes.tableFooter}>
                 <div />
                 <div>
-                    <TablePagination
-                        rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                        colSpan={3}
-                        count={rows.length}
-                        rowsPerPage={rowsPerPage}
-                        page={page}
-                        SelectProps={{
-                            inputProps: { "aria-label": "rows per page" },
-                            native: true,
-                        }}
-                        onChangePage={handleChangePage}
-                        onChangeRowsPerPage={handleChangeRowsPerPage}
-                        ActionsComponent={TablePaginationActions}
-                    />
+                    <Pagination count={numPages} page={page} onChange={handleChange} />
                 </div>
                 <div />
             </div>
