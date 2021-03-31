77438c92b30c43fcda7f42fa1b7416b55d045a67
Andrew Rutter <71574118+andlrutt@users.noreply.github.com>
2021-03-31 01:12:58 -0400

    improved inputs, re-did layout

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -1,14 +1,13 @@
-import React, { useState, useRef } from "react";
-import InputMask from "react-input-mask";
+import React from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import DescriptionIcon from "@material-ui/icons/Description";
 import Link from "@material-ui/core/Link";
 import CoreTypography from "src/components/core/typography";
 import colors from "src/components/core/colors";
 import constants from "utils/constants";
-import { Volunteer } from "utils/types";
 import { Button } from "@material-ui/core";
+import LockIcon from "@material-ui/icons/Lock";
+import PersonIcon from "@material-ui/icons/Person";
 
 function Login() {
     const styles = useStyles();
@@ -21,83 +20,113 @@ function Login() {
     return (
         <React.Fragment>
             <Container className={styles.container}>
-                <CoreTypography variant="h2" className={styles.textWrapper}>
-                    Welcome Back!
-                </CoreTypography>
+                <img
+                    src={`/${constants.org.images.banner}`}
+                    alt={`${constants.org.name.short} logo`}
+                    className={styles.banner}
+                />
+
+                <CoreTypography variant="h2">Welcome Back!</CoreTypography>
 
                 <form onSubmit={handleSubmit} className={styles.form}>
-                    <input type="email" name="email" required className={styles.input} id="emailField" />
+                    <input
+                        type="email"
+                        name="email"
+                        required
+                        placeholder="username@gmail.com"
+                        className={styles.input}
+                        id="emailField"
+                    />
+                    <PersonIcon className={styles.icon} />
 
-                    <input type="password" name="password" required className={styles.input} id="passwordField" />
+                    <input
+                        type="password"
+                        name="password"
+                        required
+                        placeholder="Enter Password"
+                        className={styles.input}
+                        id="passwordField"
+                    />
+                    <LockIcon className={styles.icon} />
 
-                    <Container className={styles.login}>
-                        <Button variant="contained" type="submit" className={styles.button}>
-                            <CoreTypography variant="button">LOGIN</CoreTypography>
-                        </Button>
-
-                        <Link>
-                            <CoreTypography variant="caption" className={styles.forgotPassword}>
-                                FORGOT PASSWORD?
-                            </CoreTypography>
-                        </Link>
-                    </Container>
+                    <Button variant="contained" type="submit" className={styles.button}>
+                        <CoreTypography variant="button">LOGIN</CoreTypography>
+                    </Button>
                 </form>
+                <Link className={styles.forgotPassword}>
+                    <CoreTypography variant="caption">FORGOT PASSWORD?</CoreTypography>
+                </Link>
             </Container>
         </React.Fragment>
     );
 }
 
+// styles
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
             display: "flex",
-            textAlign: "center",
             flexDirection: "column",
+            textAlign: "center",
             alignItems: "center",
-
-            //marginLeft: "200px",
-            //marginRight: "200px",
+            justifyContent: "center",
+            width: "900px",
         },
 
-        textWrapper: {},
+        banner: {
+            width: "250px",
+            paddingBottom: "50px",
+            paddingTop: "45px",
+        },
 
         form: {
-            paddingTop: "40px",
+            paddingTop: "20px",
+
             width: "350px",
             display: "inherit",
-            flexDirection: "column",
+            flexDirection: "inherit",
+        },
+
+        icon: {
+            position: "relative",
+            bottom: "32px",
+            left: "7px",
+            color: colors.grays["60"],
         },
 
         input: {
             height: "40px",
-            fontSize: "20px",
-            marginBottom: "40px",
+            fontSize: "17px",
+            borderStyle: "solid",
+            textIndent: "35px",
+
+            "&:focus": {
+                outline: "none",
+                borderColor: "#33A3C8", //ALSO CHANGE THIS TO BLUE ????????
+            },
         },
 
         login: {
             padding: "0",
             width: "350px",
-
-            display: "inherit",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
         },
 
         button: {
             borderRadius: "56px",
-            //width: "100%",
-            width: "130px",
-            backgroundColor: "#F58647",
+            marginTop: "30px",
+            backgroundColor: "#F58647", // CODE THIS SOMEWHERELASKDJFLKSADJFLSA;JFL;SADJFSLA;KDFJSDALKJFSALD;F
             color: colors.white,
+            "&:hover": {
+                backgroundColor: "#F58647", //AND THIS ONE SLJKDFHSLDFJHSKDLAFHKSALDFHASKDJF
+            },
         },
 
         forgotPassword: {
             color: colors.grays[80],
             cursor: "pointer",
-
-            //paddingTop: "25px",
-            paddingLeft: "35px",
+            paddingTop: "25px",
+            display: "inherit",
+            alignSelf: "center",
         },
     })
 );
