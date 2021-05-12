a79500aa862dd3f4b0b8fd4c610d5a86bc4af484
zaviermiller <zavierjmiller@gmail.com>
2021-05-12 17:53:38 -0500

    added rest of figma design for manage page

--- src/pages/admin/events/[eventId]/index.tsx
+++ src/pages/admin/events/[eventId]/index.tsx
@@ -21,16 +21,19 @@ import {
     Theme,
     createStyles,
     Switch,
+    Button,
 } from "@material-ui/core";
 import { Search } from "@material-ui/icons";
 import { styles } from "@material-ui/pickers/views/Calendar/Calendar";
 import colors from "src/components/core/colors";
 import urls from "utils/urls";
-import { markVolunteerNotPresent, markVolunteerPresent } from "server/actions/Volunteer";
+import { addVolunteer, markVolunteerNotPresent, markVolunteerPresent } from "server/actions/Volunteer";
 import VolAttendanceListItem from "src/components/VolAttendanceListItem";
+import CoreTypography from "src/components/core/typography";
+import VolQuickAddDialog from "src/components/VolQuickAddDialog";
 
 interface Props {
-    eventId: string;
+    event: Event;
     vols: PaginatedVolunteers;
 }
 
@@ -43,14 +46,47 @@ function orderVolunteers(vols: Volunteer[]): Volunteer[] {
         : [];
 }
 
-const ManageVolunteers: NextPage<Props> = ({ vols, eventId }) => {
+const ManageVolunteers: NextPage<Props> = ({ vols, event }) => {
     const styles = useStyles();
+    const [open, setOpen] = useState(false);
 
     // create EventVolunteers to easily track who signed up for what.
     const eventVols = vols.volunteers;
 
+    const createAndRegisterVol = async function (vol: Volunteer) {
+        try {
+            await addVolunteer(vol);
+            setOpen(false);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
     return (
         <Container maxWidth="xl" className={styles.container}>
+            <div className={styles.jumbotron}>
+                <Grid container spacing={0} direction="row" justify="center" style={{ width: "100%" }}>
+                    <Grid item xs={12} sm={7} lg={6} className={styles.pageTitle}>
+                        <CoreTypography variant="h1" style={{ color: "white" }}>
+                            {event.name}
+                        </CoreTypography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Grid container spacing={0} direction="row" justify="flex-end" style={{ width: "100%" }}>
+                            <Grid item xs={12} md={4}>
+                                <Button
+                                    onClick={() => {
+                                        setOpen(true);
+                                    }}
+                                    color="primary"
+                                >
+                                    Add +
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </div>
             <Grid container direction="row" spacing={6} justify="center">
                 <Grid item xs={10} md={8} lg={6}>
                     <TableContainer component={Paper}>
@@ -68,7 +104,7 @@ const ManageVolunteers: NextPage<Props> = ({ vols, eventId }) => {
                                     return (
                                         <TableRow className={styles.tr} key={i}>
                                             <VolAttendanceListItem
-                                                eventId={eventId}
+                                                eventId={event._id!}
                                                 eVol={{
                                                     present: i >= vols.registeredCount ? true : false,
                                                     volunteer: vol,
@@ -82,6 +118,13 @@ const ManageVolunteers: NextPage<Props> = ({ vols, eventId }) => {
                     </TableContainer>
                 </Grid>
             </Grid>
+            <VolQuickAddDialog
+                open={open}
+                closeDialog={() => {
+                    setOpen(false);
+                }}
+                createAndRegisterVol={createAndRegisterVol}
+            />
         </Container>
     );
 };
@@ -93,11 +136,26 @@ const useStyles = makeStyles((theme: Theme) => {
         },
         container: {
             margin: 30,
+            width: "100%",
         },
         tr: {
             cursor: "pointer",
             hover: {
                 backgroundColor: colors.lightGray,
+            },
+        },
+        jumbotron: {
+            width: "100%",
+            height: "55vh",
+            backgroundColor: theme.palette.primary.main,
+        },
+        pageTitle: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            marginTop: "20px",
+            [theme.breakpoints.down("xs")]: {
+                alignItems: "center",
             },
         },
     });
@@ -106,22 +164,24 @@ const useStyles = makeStyles((theme: Theme) => {
 export async function getServerSideProps(context: NextPageContext) {
     // get eventId from url by using context
     const eventId = context.query.eventId as string;
+    const event = await getEvent(eventId);
 
     // this func is run on server-side, so we can safely fetch the event directly
     try {
         const volsObj = await getEventVolunteers(eventId, 1);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const paginatedVols: PaginatedVolunteers = JSON.parse(JSON.stringify(volsObj));
 
         return {
             props: {
-                eventId: eventId,
+                event: event,
                 vols: paginatedVols,
             },
         };
     } catch (e) {
         return {
             props: {
-                eventId: eventId,
+                event: event,
                 vols: [],
             },
         };
